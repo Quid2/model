a5e23e8f1937698cf1c80ddc4a7503da512f79c5
Pasqualino Titto Assini <tittoassini@gmail.com>
2020-05-27 17:08:49 +0200

    test with 8.X

--- src/Data/Convertible/Tiny.hs
+++ src/Data/Convertible/Tiny.hs
@@ -1,18 +1,22 @@
-{-# LANGUAGE  MultiParamTypeClasses  ,FlexibleInstances  #-}
-module Data.Convertible.Tiny ( -- * The conversion process
-convert,
-Convertible(..),
--- * Handling the results
-ConvertResult,
-ConvertError(..),
-convError,
-prettyConvertError
-) where
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+-- | Convert across types
+module Data.Convertible.Tiny
+  ( convert
+  , Convertible(..)
+  ,-- * Handling the results
+    ConvertResult
+  , ConvertError(..)
+  , convError
+  , prettyConvertError
+  )
+where
 
-import Data.Convertible.Base
-import Data.Convertible.Instances.Text()
-import Data.Convertible.Instances.Num()
+import           Data.Convertible.Base
+import           Data.Convertible.Instances.Num ( )
+import           Data.Convertible.Instances.Text
+                                                ( )
 
 {- | Any type can be converted to itself. -}
-instance Convertible a a where safeConvert = return
-
+instance Convertible a a where
+  safeConvert = return
--- src/Data/Model/Class.hs
+++ src/Data/Model/Class.hs
@@ -6,19 +6,19 @@
 {-# LANGUAGE TypeOperators             #-}
 {-# LANGUAGE UndecidableInstances      #-}
 
-module Data.Model.Class(
-  typeModel
-  ,Model(..)
-  ,AsType(..)
+module Data.Model.Class
+  ( typeModel
+  , Model(..)
+  , AsType(..)
 
   -- *Utilities
-  ,useCT
+  , useCT
 
   -- *Re-exports
-  ,Ana
+  , Ana
   --,Typ
   )
-  where
+where
 
 import           Control.Monad
 import           Control.Monad.Trans.State.Lazy
@@ -26,7 +26,7 @@ import           Data.Either
 import           Data.Model.Env
 import           Data.Model.Types
 import           Data.Typeable
-import qualified GHC.Generics                   as G
+import qualified GHC.Generics                  as G
 import           Type.Analyse
 
 -- |Return the model for the given type
@@ -35,23 +35,26 @@ typeModel p = withEnv (asTypeP p)
 
 -- We feed to the analyser the abstracted version of the type
 -- in order to distinguish between variable and non variable positions
-asTypeP :: forall a. AsType (Ana a) => Proxy a -> State Env HType
+asTypeP :: forall a . AsType (Ana a) => Proxy a -> State Env HType
 asTypeP _ = asType (undefined :: Ana a)
 
 -- |Helper class used to capture the type parameters
 class AsType a where
   asType :: a -> State Env HType
 
-instance {-# OVERLAPPABLE #-} Model a => AsType (Typ a) where asType _ = envType (Proxy::Proxy a)
+instance {-# OVERLAPPABLE #-} Model a => AsType (Typ a) where
+  asType _ = envType (Proxy :: Proxy a)
 
 instance (AsType f,AsType a) => AsType (App f a) where
-  asType _ = TypeApp <$> asType (undefined::f) <*> asType (undefined::a)
+  asType _ = TypeApp <$> asType (undefined :: f) <*> asType (undefined :: a)
 
 --instance (KnownNat t,Typeable t) => AsType (Typ (ANat t)) where asType _ = envType (Proxy::Proxy a)
 
 -- |TypeLits are used to represent data type's parameters.
-instance (KnownNat t,Typeable t) => Model (ANat t) where
-  envType _ = return . TypeCon . TypVar . fromIntegral . anatVal $ (undefined :: ANat t)
+--instance (KnownNat t,Typeable t) => Model (ANat t) where
+instance (KnownNat t) => Model (ANat t) where
+  envType _ =
+    return . TypeCon . TypVar . fromIntegral . anatVal $ (undefined :: ANat t)
 
 
 -- | Class of types whose model can be calculated
@@ -69,24 +72,33 @@ class (Typeable a,AsType (Ana a)) => Model a where
 
 -- |Use the given constructors tree as model for the given type, returns the build type
 -- Exported so that it can be used to overwrite default definitions
-useCT :: Typeable a => Maybe (ConTree String HTypeRef) -> proxy a -> State Env (Type (TypeRef QualName))
+useCT
+  :: Typeable a
+  => Maybe (ConTree String HTypeRef)
+  -> proxy a
+  -> State Env (Type (TypeRef QualName))
 useCT ct p = addCT_ True p (return ct)
 
-addCT_ :: Typeable a => Bool -> proxy a -> State Env (Maybe (ConTree String HTypeRef)) -> State Env (Type (TypeRef QualName))
+addCT_
+  :: Typeable a
+  => Bool
+  -> proxy a
+  -> State Env (Maybe (ConTree String HTypeRef))
+  -> State Env (Type (TypeRef QualName))
 addCT_ useLocalString p mct =
-  let tr = typeRep p
-      (tc,ts) = splitTyConApp tr
+  let tr       = typeRep p
+      (tc, ts) = splitTyConApp tr
       nm hname = if useLocalString then "" else hname
       -- nm hname = hname
       uname = tyConName tc
       qname = QualName (nm $ tyConPackage tc) (nm $ tyConModule tc) uname
-  in do
-    inCtx <- enterCtx qname
-    unless inCtx $ do
-      ct <- mct
-      addDef qname $ ADT uname (fromIntegral $ length ts) ct
-    closeCtx
-    return . TypeCon . TypRef $ qname
+  in  do
+        inCtx <- enterCtx qname
+        unless inCtx $ do
+          ct <- mct
+          addDef qname $ ADT uname (fromIntegral $ length ts) ct
+        closeCtx
+        return . TypeCon . TypRef $ qname
 
 -- |Helper class, uses Generics to capture the model of a data type
 -- Adapted from the Beamable package
@@ -97,72 +109,71 @@ class GModel f where
     gtypeN :: f a -> State Env [Either HType (String,HType)]
 
 instance GModel (M1 D d V1) where
-    gcons _ = return Nothing
-    gcontree = notThere
-    gtype = notThere
-    gtypeN = notThere
+  gcons _ = return Nothing
+  gcontree = notThere
+  gtype    = notThere
+  gtypeN   = notThere
 
 -- |Datatypes with single constructor only
 --instance (GModel a, Datatype d, Constructor c) => GModel (M1 D d (M1 C c a)) where
 instance (GModel a, Constructor c) => GModel (M1 D d (M1 C c a)) where
-    gcons x = Just <$> gcontree (unM1 x)
-    gcontree = notThere
-    gtype = notThere
-    gtypeN = notThere
+  gcons x = Just <$> gcontree (unM1 x)
+  gcontree = notThere
+  gtype    = notThere
+  gtypeN   = notThere
 
 -- | Needed to avoid overlapping instances with (M1 D d (M1 C c a))
 instance (Datatype d, GModel a, GModel b) => GModel (M1 D d (a :+: b) ) where
-    gcons x = Just <$> gcontree x
-    gcontree x = ConTree <$> gcontree (unL . unM1 $ x) <*> gcontree (unR . unM1 $ x)
-    gtype = notThere
-    gtypeN = notThere
+  gcons x = Just <$> gcontree x
+  gcontree x =
+    ConTree <$> gcontree (unL . unM1 $ x) <*> gcontree (unR . unM1 $ x)
+  gtype  = notThere
+  gtypeN = notThere
 
 -- |Datatypes with multiple constructors
 instance (GModel a, Constructor c) => GModel (M1 C c a) where
   gcons = notThere
 
-  gcontree x = Con (conName x) . toE . partitionEithers  <$> gtypeN (unM1 x)
-    where
-      toE (ls,[]) = Left ls
-      toE ([],rs) = Right rs
-      toE p       = error . unwords $ ["toE: unexpected parameter",show p]
+  gcontree x = Con (conName x) . toE . partitionEithers <$> gtypeN (unM1 x)
+   where
+    toE (ls, []) = Left ls
+    toE ([], rs) = Right rs
+    toE p        = error . unwords $ ["toE: unexpected parameter", show p]
 
-  gtype = notThere
+  gtype  = notThere
   gtypeN = notThere
 
 -- |Constructors
 instance (GModel a, GModel b) => GModel (a :+: b) where
   gcons = notThere
   gcontree x = ConTree <$> gcontree (unL x) <*> gcontree (unR x)
-  gtype = notThere
+  gtype  = notThere
   gtypeN = notThere
 
 instance (Selector c,GModel a) => GModel (M1 G.S c a) where
-  gcons = notThere
+  gcons    = notThere
   gcontree = notThere
-  gtype = notThere
-  gtypeN ~s@(M1 x) = (\t -> [let n = selName s
-                               in if null n
-                                  then Left t
-                                  else Right (n,t)
-                              ]) <$> gtype x
+  gtype    = notThere
+  gtypeN ~s@(M1 x) =
+    (\t -> [let n = selName s in if null n then Left t else Right (n, t)])
+      <$> gtype x
 
 instance (GModel a, GModel b) => GModel (a :*: b) where
-  gcons = notThere
+  gcons    = notThere
   gcontree = notThere
-  gtype = notThere
+  gtype    = notThere
   gtypeN ~(x :*: y) = (++) <$> gtypeN x <*> gtypeN y
 
 instance GModel U1 where
-  gcons = notThere
+  gcons    = notThere
   gcontree = notThere
-  gtype = notThere
+  gtype    = notThere
   gtypeN _ = return []
 
 instance (AsType (Ana a),Model a) => GModel (K1 i a) where
-  gcons = notThere
+  gcons    = notThere
   gcontree = notThere
-  gtype _ = asTypeP (undefined::Proxy a)
+  gtype _ = asTypeP (undefined :: Proxy a)
   gtypeN _ = return []
 
 unL :: (l :+: r) a -> l a
